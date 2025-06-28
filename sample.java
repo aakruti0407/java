44e2a677085e1e19d02168db4288450e7f2db37a
aakruti0407 <naynapolara@gmail.com>
2025-06-28 12:08:26 +0530

    28/06/25

---
+++ sample.java
@@ -0,0 +1,12 @@
+package com.basic;
+
+public class sample {
+	
+	public static void main(String[] args) {
+		System.out.println("Welcome To Java.");
+		System.out.println("Java is Best.");
+	}
+	
+	
+
+}
