1aae496811d51023ac6dc1b5f7869f0a99765880
aakruti0407 <naynapolara@gmail.com>
2025-06-28 12:07:51 +0530

    28/06/25

---
+++ posneg.java
@@ -0,0 +1,22 @@
+package com.basic;
+
+import java.util.Scanner;
+
+public class posneg {
+	public static void main(String[] args) {
+		int a;
+		Scanner ab=new Scanner(System.in);
+		System.out.println("Enter A :");
+		a=ab.nextInt();
+		if(a>0)
+		{
+			System.out.println("A Is Positive");
+		}
+		else
+		{
+			System.out.println("A Is Negative");
+		}
+
+	}
+
+}
