ce9bc5f6575ee1ead5fbc43938c891131bc4b51a
aakruti0407 <naynapolara@gmail.com>
2025-07-01 11:06:02 +0530

    01/07/25

---
+++ simplepater.java
@@ -0,0 +1,16 @@
+package com.basic;
+
+public class simplepater {
+	public static void main(String[] args) {
+		int i,j;
+		for(i=0;i<10;i++)
+		{
+			for(j=0;j<=i;j++)
+			{
+				System.out.print("*");
+			}
+			System.out.print("\n");
+		}
+	}
+
+}
