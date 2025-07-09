9a68d4352cff2942da775849063e9b4afaad8d3e
aakruti0407 <naynapolara@gmail.com>
2025-07-09 11:33:02 +0530

    09/07/25

---
+++ derived1.java
@@ -0,0 +1,15 @@
+package com.p2;
+import com.p1.protection;
+
+
+public class derived1 extends protection {
+	public derived1() {
+		System.out.println("derived1's constructor called");
+		//System.out.println("n :"+n);
+		System.out.println("n_pub :"+n_pub);
+		System.out.println("n_pro :"+n_pro);
+		//System.out.println("n_pri :"+n_pri);
+	}
+
+
+}
