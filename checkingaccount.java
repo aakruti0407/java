5c2ad7b45f1faf2170c05b14b369a279b5f9078e
aakruti0407 <naynapolara@gmail.com>
2025-07-16 09:13:59 +0530

    16/07/25

--- checkingaccount.java
+++ checkingaccount.java
@@ -1,5 +1,30 @@
 package com.exception;
 
 public class checkingaccount {
-
+	int acno;
+	String cname;
+	double balance;
+	public checkingaccount(int acno, String cname,double balance) {
+		this.acno = acno;
+		this.cname = cname;
+		this.balance = balance;
+		System.out.println("Hello,"+cname+"\nyour account number,"+acno+" Is opened with "+balance+" Rs.");
+		
+	}
+	public void deposit(double amount) {
+		this.balance=this.balance+amount;
+	}
+	public void withdraw(double amount) throws insufficiantfund{
+		if(amount<=this.balance) {
+			this.balance=this.balance-amount;
+		}else {
+			throw new insufficiantfund(amount-this.balance);
+		}
+	}
+	public void  checkbalance()
+	{
+		System.out.println("Current balance : "+this.balance);
+	}
 }
+
+
