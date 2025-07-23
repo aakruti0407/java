36269858d7c74bf90c376a7d1f0511f7415f2cec
aakruti0407 <naynapolara@gmail.com>
2025-07-23 09:10:04 +0530

    22/07/25

---
+++ SwapTwoNumbers.java
@@ -0,0 +1,34 @@
+package Assingment;
+
+class GFG{
+
+public class SwapTwoNumbers
+{
+	int m; int n;
+	{
+		int temp = m; 
+		m = 
+		n = temp;
+		
+		System.out.println("Value of m is " + m+" and Value of n is" + n);
+	}
+	
+}
+public static void main(String[] args)
+{
+	int m = 9, n = 5;
+	
+	System.out.println("Before swap: m =" + m+ ", n =" + n);
+	int temp;
+	// --------------------------------------------------------------------------------------
+	
+	System.out.println("After swap:m =" + m + ",n ="+ n);
+	
+	swapValuesUsingThirdVariable(m,n);
+}
+private static void swapValuesUsingThirdVariable(int m, int n) {
+	
+}
+	
+
+}
